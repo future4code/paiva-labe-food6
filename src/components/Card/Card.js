6ae6308c197215c4a1cb5d6a7441156e25ddc9bb
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-06-30 16:53:40 -0300

    Inicio de carrinho

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {CardProduto,CardText,DeliverySpace} from "./style"
 
-const Card = (props) => {
+const Card = ({restaurants}) => {
     return (
         <CardProduto>
 
--- src/pages/CartPage/CardPage.js
+++ src/pages/CartPage/CardPage.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalStateContext } from '../../globalstate/GlobalStateContext';
+import CardCart from "../../components/Card/CardCart"
 
 function CardPage() {
+    const {carrinho} = useContext()
+
+    const cartList = carrinho.map((cart) => {
+        return(
+            <CardCart
+                cartProducts = {cart}
+            />
+        )
+    })
+
     return(
         <div>
             <h1>Oi eu sou a CardPage</h1>
+            
         </div>
     )
 }
